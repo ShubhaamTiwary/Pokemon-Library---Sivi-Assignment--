d68adcf1c885b40765669352818f9d6f92b4d153
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-10-07 13:33:53 +0530

    color changed

--- src/screen/RenderPokemons.js
+++ src/screen/RenderPokemons.js
@@ -9,14 +9,13 @@ const RenderPokemons = () => {
           <img src={obj.pok_url} alt={obj.pok_name} className='w-1/6 h-auto rounded-full shadow-md' />
           <div className='flex flex-col'>
             <p className='text-lg font-semibold'>{obj.pok_name}</p>
-            <button className='bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full mt-2 transition duration-300 ease-in-out transform hover:scale-105'>
+            <button className='bg-blue-400 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full mt-2 transition duration-300 ease-in-out transform hover:scale-105'>
               Save Pokémon
             </button>
           </div>
         </div>
       ))}
     </div>
-
   )
 }
 
